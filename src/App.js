04aaaf2df815f6a8b0ccbe115ac77c9756809985
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2023-12-26 12:39:19 +0100

    add6

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div>
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Logo</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Logo</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul className="navbar-nav">
+              <li className="nav-item">
                 <Link className='nav-link' to="/">Produits</Link>
               </li>
          
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link className='nav-link' to="/categories/create">Add categorie</Link>
               </li>
 
@@ -49,4 +49,9 @@ export default App
  * login : https://www.bahmedkamel.com/?rest_route=/simple-jwt-login/v1/auth&username=admin&password=pass@pass
  * 
  * jwt : eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDM1ODcyMjYsImV4cCI6MTcwMzU5MDgyNiwiZW1haWwiOiJhZG1pbkBiYWhtZWRrYW1lbC5jb20iLCJpZCI6IjEiLCJzaXRlIjoiaHR0cHM6XC9cL3d3dy5iYWhtZWRrYW1lbC5jb20iLCJ1c2VybmFtZSI6ImFkbWluIiwiaXNzIjoiaHR0cHM6XC9cL3d3dy5iYWhtZWRrYW1lbC5jb20ifQ.j6HAG32DZ0GJn6P_yxpncEbwQJ4NPYGR4Dh_7wH5aW4
+ */
+
+/**
+ * 
+ * learn about "json-server" to create fast api  (checkout doc for more !)
  */
--- src/pages/Produits.jsx
+++ src/pages/Produits.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function Produits() {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDM1ODcyMjYsImV4cCI6MTcwMzU5MDgyNiwiZW1haWwiOiJhZG1pbkBiYWhtZWRrYW1lbC5jb20iLCJpZCI6IjEiLCJzaXRlIjoiaHR0cHM6XC9cL3d3dy5iYWhtZWRrYW1lbC5jb20iLCJ1c2VybmFtZSI6ImFkbWluIiwiaXNzIjoiaHR0cHM6XC9cL3d3dy5iYWhtZWRrYW1lbC5jb20ifQ.j6HAG32DZ0GJn6P_yxpncEbwQJ4NPYGR4Dh_7wH5aW4"
+        const config = {
+            headers: { Authorization: 'Bearer ' + token }
+        };
+        axios.get('https://www.bahmedkamel.com/wp-json/wc/v3/products', config)
+            .then(response => {
+                console.log(response.data);
+                setProducts(response.data)
+            })
+            .catch((error) => console.log(error))
+    }, [])
+
     return (
         <div className="container mt-3">
 
@@ -16,7 +31,11 @@ function Produits() {
                     </tr>
                 </thead>
                 <tbody>
-
+                    {products.map((p,i)=>(<tr key={i}>
+                        <td> <img src={p.images[0].src} width={50} height={50} alt="" /></td>
+                        <td>{p.name}</td>
+                        <td>{p.price}</td>
+                    </tr>))}
                 </tbody>
             </table>
         </div>
